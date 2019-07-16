6aff788d06c2191b19fc06cef2e6d925b2b08265
RajuKoushik <g.rajukoushik@gmail.com>
2019-07-16 05:14:56 -0700

    Updated the user registration module

--- config/db.js
+++ config/db.js
@@ -1,23 +1,19 @@
 const mongoose = require('mongoose');
-
 const config = require('config');
-
 const db = config.get('mongoURI');
-
-// const connectDB = mongoose.connect(db);
-
-//connecting the mongoDB teh async way
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
-    console.log('MongoDB connected..');
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+
+      useCreateIndex: true
+    });
+
+    console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-
-    //Exit process with failure
+    // Exit process with failure
     process.exit(1);
   }
 };
-
 module.exports = connectDB;
