4dd1aa88b7ac5a9d6692326b189016bc14bc20d3
RajuKoushik <g.rajukoushik@gmail.com>
2019-07-16 07:43:55 -0700

    Added the implementation of jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+const jwt = require('jsonwebtoken');
+
+const config = require('config');
 
 // @route    GET api/users
 // @desc     Test route
@@ -60,8 +63,21 @@ router.post(
 
       await user.save();
 
-      // Return jsonwebtoken
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
